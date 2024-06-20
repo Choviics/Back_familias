1230f66f3de3d8d7cba2a298c88df36482fd5711
Choviics <152323865+Choviics@users.noreply.github.com>
2024-06-20 12:55:11 -0400

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ import mysql from "mysql2";
 
 
 export const pool = mysql.createConnection({
-    host: '127.0.0.1',
+    host: '',
     port:'3306',
-    user: 'Rokilo',
-    password: 'Enriquenercasseau2300',
-    database: 'familias_power'
+    user: '',
+    password: '',
+    database: ''
   });
